511d7632f12db9df445f9efb3c8647f69997171c
Brendan Quinn <brendan-quinn@northwestern.edu>
2022-08-05 18:59:11 +0000

    Initial pass at pre-filtering search queries
    Co-authored-by: Karen Shaw <karendid@gmail.com>

--- src/api/request/pipeline.js
+++ src/api/request/pipeline.js
@@ -1,6 +1,6 @@
-class RequestPipeline {
+module.exports = class RequestPipeline {
   constructor(body) {
-    this.body = body;
+    this.body = JSON.parse(body);
   }
 
   // Things tranformer needs to do:
@@ -8,7 +8,21 @@ class RequestPipeline {
   // - Reading room/IP (not in first iteration)
   // - Add `track_total_hits` to body of search (so we can get accurate hits.total.value)
 
+  authFilter() {
+    const matchTheQuery = this.body.query || { match_all: {} };
+    const beUnpublished = { term: { published: false } };
+    const beRestricted = { term: { visibility: "Private" } };
+
+    this.body.query = {
+      bool: {
+        must: [matchTheQuery],
+        must_not: [beUnpublished, beRestricted],
+      },
+    };
+    return this;
+  }
+
   toJson() {
     return JSON.stringify(this.body);
   }
-}
+};
