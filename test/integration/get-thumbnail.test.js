c5bd14ee2e833caa1d2484473778f1aaffc31f86
Michael B. Klein <mbklein@gmail.com>
2022-08-29 15:07:46 +0000

    Add size and aspect parameters to thumbnail request

--- test/integration/get-thumbnail.test.js
+++ test/integration/get-thumbnail.test.js
@@ -7,12 +7,10 @@ const { handler } = require("../../src/handlers/get-work-thumbnail");
 describe("Work thumbnail", () => {
   helpers.saveEnvironment();
   const mock = helpers.mockIndex();
-
   const event = helpers
     .mockEvent("GET", "/works/{id}/thumbnail")
     .pathPrefix("/api/v2")
-    .pathParams({ id: 1234 })
-    .render();
+    .pathParams({ id: 1234 });
 
   beforeEach(() => {
     process.env.API_TOKEN_SECRET = "abcdef";
@@ -23,12 +21,12 @@ describe("Work thumbnail", () => {
       .get("/dc-v2-work/_doc/1234")
       .reply(200, helpers.testFixture("mocks/work-1234.json"));
     mock
-      .get("/iiif/2/thumbnail/test")
-      .reply(200, helpers.testFixture("mocks/thumbnail.jpg"), {
+      .get("/iiif/2/mbk-dev/5678/square/!300,300/0/default.jpg")
+      .reply(200, helpers.testFixture("mocks/thumbnail_square.jpg"), {
         "Content-Type": "image/jpeg",
       });
 
-    const result = await handler(event);
+    const result = await handler(event.render());
     expect(result.statusCode).to.eq(200);
     expect(result.headers["content-type"]).to.eq("image/jpeg");
   });
@@ -38,10 +36,10 @@ describe("Work thumbnail", () => {
       .get("/dc-v2-work/_doc/1234")
       .reply(200, helpers.testFixture("mocks/work-1234.json"));
     mock
-      .get("/iiif/2/thumbnail/test")
+      .get("/iiif/2/mbk-dev/5678/square/!300,300/0/default.jpg")
       .reply(403, "Forbidden", { "Content-Type": "text/plain" });
 
-    const result = await handler(event);
+    const result = await handler(event.render());
     expect(result.statusCode).to.eq(403);
     expect(result.body).to.eq("Forbidden");
   });
@@ -51,7 +49,7 @@ describe("Work thumbnail", () => {
       .get("/dc-v2-work/_doc/1234")
       .reply(200, helpers.testFixture("mocks/missing-work-1234.json"));
 
-    const result = await handler(event);
+    const result = await handler(event.render());
     expect(result.statusCode).to.eq(404);
   });
 
@@ -60,7 +58,68 @@ describe("Work thumbnail", () => {
       .get("/dc-v2-work/_doc/1234")
       .reply(200, helpers.testFixture("mocks/work-1234-no-thumbnail.json"));
 
-    const result = await handler(event);
+    const result = await handler(event.render());
     expect(result.statusCode).to.eq(404);
+  });
+
+  it("accepts a proper size", async () => {
+    mock
+      .get("/dc-v2-work/_doc/1234")
+      .reply(200, helpers.testFixture("mocks/work-1234.json"));
+    mock
+      .get("/iiif/2/mbk-dev/5678/square/!200,200/0/default.jpg")
+      .reply(200, helpers.testFixture("mocks/thumbnail_square.jpg"), {
+        "Content-Type": "image/jpeg",
+      });
+
+    const result = await handler(event.queryParams({ size: 200 }).render());
+    expect(result.statusCode).to.eq(200);
+  });
+
+  it("rejects invalid sizes", async () => {
+    let result = await handler(event.queryParams({ size: "foo" }).render());
+    expect(result.statusCode).to.eq(400);
+    expect(result.body).to.contain("foo is not");
+
+    result = await handler(event.queryParams({ size: 500 }).render());
+    expect(result.statusCode).to.eq(400);
+    expect(result.body).to.contain("500px");
+  });
+
+  it("accepts proper aspect ratios", async () => {
+    mock
+      .get("/dc-v2-work/_doc/1234")
+      .times(2)
+      .reply(200, helpers.testFixture("mocks/work-1234.json"));
+
+    let resultFixture = "mocks/thumbnail_full.jpg";
+    mock
+      .get("/iiif/2/mbk-dev/5678/full/!300,300/0/default.jpg")
+      .reply(200, helpers.testFixture(resultFixture), {
+        "Content-Type": "image/jpeg",
+      });
+
+    let result = await handler(event.queryParams({ aspect: "full" }).render());
+    expect(result.statusCode).to.eq(200);
+    let expected = helpers.encodedFixture(resultFixture);
+    expect(result.body).to.eq(expected);
+
+    resultFixture = "mocks/thumbnail_square.jpg";
+    mock
+      .get("/iiif/2/mbk-dev/5678/square/!300,300/0/default.jpg")
+      .reply(200, helpers.testFixture(resultFixture), {
+        "Content-Type": "image/jpeg",
+      });
+
+    result = await handler(event.queryParams({ aspect: "square" }).render());
+    expect(result.statusCode).to.eq(200);
+    expected = helpers.encodedFixture(resultFixture);
+    expect(result.body).to.eq(expected);
+  });
+
+  it("rejects improper aspect ratio", async () => {
+    const result = await handler(event.queryParams({ aspect: "foo" }).render());
+    expect(result.statusCode).to.eq(400);
+    expect(result.body).to.contain("Unknown aspect ratio: foo");
   });
 });
