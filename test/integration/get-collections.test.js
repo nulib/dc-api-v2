3942a7350060dd1c6d644df657e9e20a47c1eefa
Michael B. Klein <mbklein@gmail.com>
2022-10-07 20:12:38 +0000

    Remove searchToken from /collections route Also add support for size parameter

--- test/integration/get-collections.test.js
+++ test/integration/get-collections.test.js
@@ -13,32 +13,33 @@ describe("Collections route", () => {
     const handler = getCollectionsHandler.handler;
     const originalQuery = { size: 10, from: 0 };
     const authQuery = new RequestPipeline(originalQuery).authFilter().toJson();
-    const searchToken =
-      "N4IgRg9gJgniBcoDOBLAXgUwQRgAwF8AaEAW2gwBskEBtEAYwgoo3oBcUIA7agXXyA";
+    const baseEvent = helpers
+      .mockEvent("GET", "/collections")
+      .pathPrefix("/api/v2");
 
-    it("requires a valid searchToken", async () => {
-      const event = helpers
-        .mockEvent("GET", "/collections")
-        .pathPrefix("/api/v2")
-        .queryParams({ searchToken: "Ceci n'est pas une searchToken" })
-        .render();
+    describe("validates parameters", () => {
+      it("page", async () => {
+        const event = baseEvent.queryParams({ page: 0 }).render();
+        const result = await handler(event);
+        expect(result.statusCode).to.eq(400);
+        const resultBody = JSON.parse(result.body);
+        expect(resultBody.message).to.eq("page must be >= 1");
+      });
 
-      const result = await handler(event);
-      expect(result.statusCode).to.eq(400);
-      const resultBody = JSON.parse(result.body);
-      expect(resultBody.message).to.eq("searchToken is invalid");
+      it("size", async () => {
+        const event = baseEvent.queryParams({ size: 0 }).render();
+        const result = await handler(event);
+        expect(result.statusCode).to.eq(400);
+        const resultBody = JSON.parse(result.body);
+        expect(resultBody.message).to.eq("size must be >= 1");
+      });
     });
 
     it("paginates results using a searchToken and page number", async () => {
       mock
         .post("/dc-v2-collection/_search", authQuery)
         .reply(200, helpers.testFixture("mocks/collections.json"));
-      const event = helpers
-        .mockEvent("GET", "/collections")
-        .pathPrefix("/api/v2")
-        .queryParams({ searchToken, page: 1 })
-        .body(authQuery)
-        .render();
+      const event = baseEvent.queryParams({ page: 1 }).render();
       const result = await handler(event);
       expect(result.statusCode).to.eq(200);
       expect(result.headers).to.include({ "content-type": "application/json" });
