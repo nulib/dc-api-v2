a66219098adc2573034e744a5c88cc3875a4f941
Michael B. Klein <mbklein@gmail.com>
2022-10-31 16:03:41 +0000

    Workaround for NUSSO missing directory attribute issue

--- src/handlers/get-auth-callback.js
+++ src/handlers/get-auth-callback.js
@@ -51,6 +51,40 @@ async function redeemSsoToken(event) {
       const user = response.data.results[0];
       return user;
     } catch (err) {
+      if ((err = ~/Reason: ResponseCode 404 is treated as error/)) {
+        return await redeemForNetIdOnly(event);
+      }
+      console.error(err);
+      return null;
+    }
+  } else {
+    console.warn("No NUSSO token found in request");
+    return null;
+  }
+}
+
+async function redeemForNetIdOnly(event) {
+  if (event.cookieObject.nusso) {
+    try {
+      const response = await axios.get(
+        `${process.env.NUSSO_BASE_URL}validateWebSSOToken`,
+        {
+          headers: {
+            apikey: process.env.NUSSO_API_KEY,
+            webssotoken: event.cookieObject.nusso,
+          },
+        }
+      );
+      const { netid } = response.data;
+      const user = {
+        uid: netid,
+        displayName: netid,
+        givenName: netid,
+        sn: "(NetID)",
+        mail: `${netid}@e.northwestern.edu`,
+      };
+      return user;
+    } catch (err) {
       console.error(err);
       return null;
     }
