094e892bcd31e6124a2c1520ab1a3572aaac9d7e
Brendan Quinn <brendan-quinn@northwestern.edu>
2023-06-23 19:13:17 +0000

    Turn a POSTed question into a valid JSON response from the LLM

--- python/src/handlers/chat.py
+++ python/src/handlers/chat.py
@@ -2,12 +2,65 @@ import base64
 import json
 import os
 import setup
+from langchain.chains import RetrievalQAWithSourcesChain
+
+
+
 
 def handler(event, context):
+  if not is_authenticated(event):
+    return {
+    "statusCode": 401,
+    "headers": {
+      "Content-Type": "text/plain"
+    },
+    "body": "Unauthorized"
+  }
+  question = get_query(event)
+  index_name = get_param(event, "index", "Work")
+  text_key = get_param(event, "text_key", "title")  
+  attributes = get_param(event, 
+                         "attributes", 
+                         "identifier,title,source,alternate_title,contributor,create_date,creator,date_created,description,genre,keywords,language,location,physical_description_material,physical_description_size,scope_and_contents,style_period,subject,table_of_contents,technique,work_type").split(",")
+
+  weaviate = setup.weaviate_vector_store(index_name=index_name, 
+                                         text_key=text_key, 
+                                         attributes=attributes)
+  
+  client = setup.openai_chat_client()
+
+
+  chain = RetrievalQAWithSourcesChain.from_chain_type(
+    client, 
+    chain_type="stuff", 
+    retriever=weaviate.as_retriever(),
+    return_source_documents=True)
+
+  response = chain({"question": question})
+  print(response)
+  response['source_documents'] = [doc.__dict__ for doc in response['source_documents']]
+  return {
+    "statusCode": 200,
+    "headers": {
+      "Content-Type": "application/json"
+    },
+    "body": json.dumps(response)
+  }
+
+
+def get_param(event, parameter, default):
+  params = event.get("queryStringParameters", {})
+  return params.get(parameter, default)
+
+
+def get_query(event):
   question = event.get("body", "")
   if event.get("isBase64Encoded", False):
     question = base64.b64decode(question)
+  return question
 
+
+def is_authenticated(event):
   headers = event.get("headers")
   token = headers.get("authorization", headers.get("Authorization", None))
 
@@ -18,32 +71,10 @@ def handler(event, context):
         token = v
   else:
     token = token.replace("Bearer ", "")
-
-  if not setup.validate_token(token):
-    return {
-    "statusCode": 401,
-    "headers": {
-      "Content-Type": "text/plain"
-    },
-    "body": "Unauthorized"
-  }
+  
+  return setup.validate_token(token)
 
 
-  params = event.get("queryStringParameters", {})
-  index_name = params.get("index", "Work")
-  text_key = params.get("text_key", "title")
-  attributes = params.get("attributes", "identifier,title").split(",")
 
-  weaviate = setup.weaviate_vector_store(index_name=index_name, 
-                                         text_key=text_key, 
-                                         attributes=attributes)
-  result = weaviate.similarity_search_by_text(query=question, 
-                                              additional="certainty")
-
-  return {
-    "statusCode": 200,
-    "headers": {
-      "Content-Type": "application/json"
-    },
-    "body": json.dumps([doc.__dict__ for doc in result])
-  }
+# result = weaviate.similarity_search_by_text(query=question, 
+#                                             additional="certainty")
