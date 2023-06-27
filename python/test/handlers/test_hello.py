2435b29b8fef4c56fcf0d8b460b6573b85331c0d
Michael B. Klein <mbklein@gmail.com>
2023-06-27 17:14:09 +0000

    Split event and token handling into helpers
    Add python tests & fix code style
    
    Build document prompt dynamically using requested attributes

--- python/test/handlers/test_hello.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from src.handlers import hello
-
-class TestFunction(unittest.TestCase):
-  def test_function(self):
-    event = {'queryStringParameters': {'name': 'Joe'}}
-    context = {'requestid' : '1234'}
-    result = hello.lambda_handler(event, context)
-    self.assertEqual(result['body'], 'Hello, Joe')
