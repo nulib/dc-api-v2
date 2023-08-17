a901d54dcf740a1d105e072bdf1230a6bdfd7a30
Brendan Quinn <brendan-quinn@northwestern.edu>
2023-08-17 02:45:03 +0000

    Update the prompts for updated schema, try CORS fix

--- python/src/helpers/prompts.py
+++ python/src/helpers/prompts.py
@@ -1,116 +1,188 @@
 # ruff: noqa: E501
 def prompt_template(): 
-  return """Given the following list of sources, create a helpful answer to the supplied question.
-    If you don't know the answer, just say that you don't know. Don't try to make up an answer.
-    Don't include sources in the answer.
+  return """Using all of the provided source documents, create a helpful and thorough answer to the supplied question.
+  If you don't know the answer, just say that you don't know. Don't try to make up an answer, but you should use the documents provided in order to ground your response.
+  It may be helpful to explain why a provided document does not pertain to the query as well.
+  Feel free to reference various aspects of the sources in your explanation, but please don't include the full sources in the answer.
+  The Content field represents the title of each document, and the Metadata fields are the attributes. The Source field is the unique identifier for each document.
+  'certainty' is an opinionated measure of the distance between the query vector and the document embedding vector. Certainty always returns a number between 0 and 1, with 1 indicating identical vectors and 0 indicating opposing angles.
 
-    Content: 10th Annual Berkeley Folk Music Festival brochure
-    Metadata: 
-      alternate_title: ['Tenth Annual Berkeley Folk Music Festival brochure']
-      contributor: ['University of California, Berkeley. Associated Students', 'Olivier, Barry, 1935-', 'Herrero, Lowell, 1921-']
-      create_date: 2021-03-16T17:07:28.356507Z
+    Content: Purchase order and note
+    Metadata:
+      _additional: {{'certainty': 0.8744078576564789, 'id': '29389b8d-a85d-46d1-9a6d-a738c6f81c88'}}
+      accession_number: BFMF_B24_F03_016
+      alternate_title: None
+      api_model: Work
+      catalog_key: None
+      collection: Berkeley Folk Music Festival
+      contributor: ['University of California, Berkeley. Associated Students', 'Berkeley Folk Music Festival']
+      create_date: 2021-03-19T22:22:33.902972Z
       creator: None
-      date_created: ['1967']
-      description: ['Brochure for the 10th Annual Berkeley Folk Music Festival, June 30 - July 4, 1967 at the University of California, Berkeley. Includes event schedule, photos and brief descriptions of participating artists and folklorists, and ticket information. Front cover features a Lowell Herrero illustration of Uncle Sam playing guitar.']
-      genre: ['brochures']
+      date_created: ['October 7, 1970', '1970?']
+      description: ['Purchase order for costs related to security for the 1970 Berkeley Folk Music Festival and a handwritten note containing calculations and the heading "Police"']
+      genre: ['notes (documents)', 'purchase orders']
+      identifier_descriptive: ['MS 63']
       keywords: None
       language: ['English']
+      library_unit: Charles Deering McCormick Library of Special Collections
       location: None
       physical_description_material: None
-      physical_description_size: ['11 inches (height) x 4.25 inches (width)', '11 inches (height) x 16.75 inches (width)', '11 inches (height) x 8.5 inches (width)']
+      physical_description_size: ['5 inches (height) x 3 inches (width)', '7 inches (height) x 8.5 inches (width)']
+      preservation_level: Level 1
+      published: True
+      related_material: None
+      related_url: None
+      rights_holder: None
+      rights_statement: In Copyright
       scope_and_contents: None
+      series: ['Berkeley Folk Music Festival Archive--3. Festivals: Records, Budgets, Publicity']
+      source: 29389b8d-a85d-46d1-9a6d-a738c6f81c88
+      status: Done
       style_period: None
-      subject: ['Berkeley (Calif.)', 'University of California, Berkeley', 'Davis, Gary, 1896-1972', 'James Cotton Blues Band', 'Havens, Richie', 'Ian, Janis', 'Thomas, Tony, 1913-', 'Watson, Doc', 'Darlington, Sandy', 'Steve Miller Blues Band', 'Green, Archie', 'Hinton, Sam, 1917-2009', 'Charles River Valley Boys (Musical group)', 'Crome Syrcus (Musical group)', 'Kaleidoscope (Musical group)', 'Marshall, Charley', 'Berkeley Folk Music Festival (10th : 1967 : Berkeley, Calif.)', 'Spector, Phil', 'Von Meier, Kurt', 'Gleason, Ralph J.', 'Country Joe and the Fish', 'Red Crayola (Musical group)', 'Goodfellow, Robin, 1940-2017', 'Darlington, Jeanie', 'Cleanliness and Godliness Skiffle Band', 'Tarlton, Jimmie, 1892-1979']
+      subject: ['Berkeley Folk Music Festival (15th : 1970 : Berkeley, Calif.)']
       table_of_contents: None
       technique: None
+      visibility: Public
       work_type: Image
-    Source: ce3ca54a-d928-43b8-bd1f-0b867e1fd624
+    Source: 29389b8d-a85d-46d1-9a6d-a738c6f81c88
     
-
-
-    Content: University of California Folk Music Festival
-    Metadata: 
+    Content: Berkeley Folk Music Festival, 1966 June 26-30
+    Metadata:
+      _additional: {{'certainty': 0.869585394859314, 'id': '477e3f63-fc06-4bfc-8734-0b6100c0d1c3'}}
+      accession_number: BFMF_P34_09
       alternate_title: None
-      contributor: ['Olivier, Barry, 1935-']
-      create_date: 2021-03-16T13:09:52.290256Z
+      api_model: Work
+      catalog_key: None
+      collection: Berkeley Folk Music Festival
+      contributor: ['Olivier, Barry, 1935-', 'Hart, Kelly, 1943-', 'University of California, Berkeley. Associated Students']
+      create_date: 2021-03-15T20:20:06.428594Z
       creator: None
-      date_created: ['1961?']
-      description: ['Description of the Berkeley Folk Music Festival, and the financial arrangements between Festival organizers and the Committee for Arts and Lectures']
-      genre: ['descriptions (documents)', 'financial records']
+      date_created: ['1966']
+      description: ['Poster for the Berkeley Folk Music Festival, held at the University of California, Berkeley from June 30 to July 4, 1966, presented by the Associated Students. White text on black background between black and white images of a man playing a fiddle and another man singing into a mic while holding a guitar. Guest list includes Pete Seeger, Jefferson Airplane, Sam Hinton, Greenbriar Boys, Shlomo Carlebach, John Fahey, Los Halcones de Salitrillos, Charley Marshall, Phil Ochs, Ralph J. Gleason, Malvina Reynolds, Robert Pete Williams, Alice Stuart Thomas, Bess Lomax Hawes, and Charles Seeger.']
+      genre: ['posters']
+      identifier_descriptive: None
       keywords: None
       language: ['English']
+      library_unit: Charles Deering McCormick Library of Special Collections
       location: None
       physical_description_material: None
-      physical_description_size: ['11 inches (height) x 8.5 inches (width)']
+      physical_description_size: ['12.75 inches (height) x 12.75 inches (width)']
+      preservation_level: Level 1
+      published: True
+      related_material: None
+      related_url: None
+      rights_holder: None
+      rights_statement: In Copyright
       scope_and_contents: None
+      series: ['Berkeley Folk Music Festival Archive--13. Miscellaneous Posters']
+      source: 477e3f63-fc06-4bfc-8734-0b6100c0d1c3
+      status: Done
       style_period: None
-      subject: ['Berkeley Folk Music Festival', 'University of California, Berkeley. Committee on Drama, Lectures, and Music']
+      subject: ['Berkeley (Calif.)', 'University of California, Berkeley', 'Gleason, Ralph J.', 'Folk music', 'Jefferson Airplane (Musical group)', 'Seeger, Pete, 1919-2014', 'Fahey, John, 1939-2001', 'Williams, Robert Pete, 1914-1980', 'Folk music festivals', 'Hinton, Sam, 1917-2009', 'Reynolds, Malvina', 'Halcones de Salitrillo (Musical group)', 'Folk musicians', 'Concerts', 'Carlebach, Shlomo, 1925-1994', 'Marshall, Charley', 'Ochs, Phil', 'Seeger, Charles, 1886-1979', 'Berkeley Folk Music Festival', 'Greenbriar Boys', 'Stuart, Alice, 1942-', 'Hawes, Bess Lomax, 1921-2009']
       table_of_contents: None
       technique: None
+      visibility: Public
       work_type: Image
-    Source: a2979042-4e76-4808-ab1a-5eb0bd39ffda
-    
+    Source: 477e3f63-fc06-4bfc-8734-0b6100c0d1c3
 
-
-    Content: Mitch Greenhill
-    Metadata: 
+    Content: Berkeley Folk Music Festival, 1966 June 26-30
+    Metadata:
+      _additional: {{'certainty': 0.8694239258766174, 'id': 'bddeb375-762b-45e3-9e4e-5a4084ac5955'}}
+      accession_number: BFMF_P13_01
       alternate_title: None
-      contributor: ['Olivier, Barry, 1935-']
-      create_date: 2021-03-16T21:54:44.763820Z
+      api_model: Work
+      catalog_key: None
+      collection: Berkeley Folk Music Festival
+      contributor: ['Olivier, Barry, 1935-', 'Hart, Kelly, 1943-', 'University of California, Berkeley. Associated Students']
+      create_date: 2021-03-16T15:32:04.982281Z
       creator: None
-      date_created: ['circa 1958 to circa 1970']
-      description: ['Cut out photo of Mitch Greenhill playing guitar in an outdoor setting at the San Diego Folk Festival. Back includes performance schedule at the Berkeley Folk Music Festival.']
-      genre: ['black-and-white photographs']
-      keywords: None
-      language: None
-      location: None
-      physical_description_material: None
-      physical_description_size: ['7.5 inches (height) x 4.75 inches (width)']
-      scope_and_contents: None
-      style_period: None
-      subject: ['San Diego (Calif.)', 'Greenhill, Mitch', 'San Diego Folk Festival']
-      table_of_contents: None
-      technique: None
-      work_type: Image
-    Source: f2ee01cf-ebe9-44b2-8293-b2f98f982aea
-    
-
-
-    Content: Purchase order and invoice
-    Metadata: 
-      alternate_title: None
-      contributor: ['Berkeley Folk Music Festival', 'University of California, Berkeley. Associated Students']
-      create_date: 2021-03-15T16:22:45.637252Z
-      creator: None
-      date_created: ['July 13, 1962 and July 30, 1962']
-      description: ['ASUC purchase order issued on behalf of the Berkeley Folk Music Festival to the Regents of the University of California, with accompanying invoice']
-      genre: ['purchase orders']
+      date_created: ['1966']
+      description: ['Poster for the Berkeley Folk Music Festival, held at the University of California, Berkeley from June 30 to July 4, 1966, presented by the Associated Students. White text on black background between black and white images of a man playing a fiddle and another man singing into a mic while holding a guitar. Guest list includes Pete Seeger, Jefferson Airplane, Sam Hinton, Greenbriar Boys, Shlomo Carlebach, John Fahey, Los Halcones de Salitrillos, Charley Marshall, Phil Ochs, Ralph J. Gleason, Malvina Reynolds, Robert Pete Williams, Alice Stuart Thomas, Bess Lomax Hawes, and Charles Seeger.']
+      genre: ['posters']
+      identifier_descriptive: None
       keywords: None
       language: ['English']
+      library_unit: Charles Deering McCormick Library of Special Collections
       location: None
       physical_description_material: None
-      physical_description_size: ['5.5 inches (height) x 8.5 inches (width)', '7 inches (height) x 8.5 inches (width)']
+      physical_description_size: ['13.75 inches (height) x 21.75 inches (width)']
+      preservation_level: Level 1
+      published: True
+      related_material: None
+      related_url: None
+      rights_holder: None
+      rights_statement: In Copyright
       scope_and_contents: None
+      series: ['Berkeley Folk Music Festival Archive--9. Posters of Berkeley Folk Music Festivals']
+      source: bddeb375-762b-45e3-9e4e-5a4084ac5955
+      status: Done
       style_period: None
-      subject: ['University of California (System). Regents', 'Berkeley Folk Music Festival (5th : 1962 : Berkeley, Calif.)']
+      subject: ['Berkeley (Calif.)', 'University of California, Berkeley', 'Gleason, Ralph J.', 'Folk music', 'Jefferson Airplane (Musical group)', 'Seeger, Pete, 1919-2014', 'Fahey, John, 1939-2001', 'Williams, Robert Pete, 1914-1980', 'Folk music festivals', 'Hinton, Sam, 1917-2009', 'Reynolds, Malvina', 'Halcones de Salitrillo (Musical group)', 'Folk musicians', 'Concerts', 'Carlebach, Shlomo, 1925-1994', 'Marshall, Charley', 'Ochs, Phil', 'Berkeley Folk Music Festival (9th : 1966 : Berkeley, Calif.)', 'Hawes, Bess Lomax, 1921-2009', 'Greenbriar Boys', 'Stuart, Alice, 1942-', 'Seeger, Charles, 1886-1979', 'Berkeley Folk Music Festival']
       table_of_contents: None
       technique: None
+      visibility: Public
       work_type: Image
-    Source: 0df68024-5b07-46d8-87b3-2efc6b22f9d7
+    Source: bddeb375-762b-45e3-9e4e-5a4084ac5955
 
-    QUESTION: What venues did musicians play at during the Berkeley Folk Music Festival?
-    HELPFUL ANSWER: Based on the provided documents, here are the venues that were used for performances by musicians at the Berkeley Folk Music Festival:
+    Content: Berkeley Folk Music Festival, 1966 June 30-July 4
+    Metadata:
+      _additional: {{'certainty': 0.8693937957286835, 'id': 'aab0bb76-ab02-429a-843a-5be56e31ba67'}}
+      accession_number: BFMF_P22_16
+      alternate_title: None
+      api_model: Work
+      catalog_key: None
+      collection: Berkeley Folk Music Festival
+      contributor: ['Olivier, Barry, 1935-', 'Hart, Kelly, 1943-', 'University of California, Berkeley. Associated Students']
+      create_date: 2021-03-16T13:46:47.559482Z
+      creator: None
+      date_created: ['1966']
+      description: ['Poster for the 9th Annual Berkeley Folk Music Festival, held at the University of California, Berkeley from June 30 to July 4, 1966, presented by the Associated Students. White text on black background between black and white images of a man playing a fiddle and another man singing into a mic while holding a guitar. Guest list includes Pete Seeger, Jefferson Airplane, Sam Hinton, Greenbriar Boys, Shlomo Carlebach, John Fahey, Los Halcones de Salitrillos, Charley Marshall, Phil Ochs, Ralph J. Gleason, Malvina Reynolds, Robert Pete Williams, Alice Stuart Thomas, Bess Lomax Hawes, and Charles Seeger. Originally found in box 28, folder 3.']
+      genre: ['posters']
+      identifier_descriptive: None
+      keywords: None
+      language: ['English']
+      library_unit: Charles Deering McCormick Library of Special Collections
+      location: None
+      physical_description_material: None
+      physical_description_size: ['24.25 inches (height) x 37.5 inches (width)']
+      preservation_level: Level 1
+      published: True
+      related_material: None
+      related_url: None
+      rights_holder: None
+      rights_statement: In Copyright
+      scope_and_contents: None
+      series: ['Berkeley Folk Music Festival Archive--13. Miscellaneous Posters']
+      source: aab0bb76-ab02-429a-843a-5be56e31ba67
+      status: Done
+      style_period: None
+      subject: ['Berkeley (Calif.)', 'University of California, Berkeley', 'Gleason, Ralph J.', 'Folk music', 'Jefferson Airplane (Musical group)', 'Seeger, Pete, 1919-2014', 'Fahey, John, 1939-2001', 'Williams, Robert Pete, 1914-1980', 'Folk music festivals', 'Hinton, Sam, 1917-2009', 'Reynolds, Malvina', 'Halcones de Salitrillo (Musical group)', 'Folk musicians', 'Concerts', 'Carlebach, Shlomo, 1925-1994', 'Marshall, Charley', 'Ochs, Phil', 'Berkeley Folk Music Festival (9th : 1966 : Berkeley, Calif.)', 'Hawes, Bess Lomax, 1921-2009', 'Greenbriar Boys', 'Stuart, Alice, 1942-', 'Seeger, Charles, 1886-1979', 'Berkeley Folk Music Festival']
+      table_of_contents: None
+      technique: None
+      visibility: Public
+      work_type: Image
+    Source: aab0bb76-ab02-429a-843a-5be56e31ba67
 
-  1. Fourth Annual Berkeley Folk Music Festival (1961):
-    - Pauley Ballroom and Greek Theatre
+    QUESTION: Which musicians played at the Berkeley Folk Music Festival?
+    HELPFUL ANSWER: For the 1966 Berkeley Folk Music Festival, held at the University of California, Berkeley from June 30 to July 4, the following musicians and groups were listed as performers:
 
-  2. Fifth Annual Berkeley Folk Music Festival (1962):
-    - No specific venue mentioned in the provided document.
+    Pete Seeger
+    Jefferson Airplane
+    Sam Hinton
+    Greenbriar Boys
+    Shlomo Carlebach
+    John Fahey
+    Los Halcones de Salitrillos
+    Charley Marshall
+    Phil Ochs
+    Ralph J. Gleason
+    Malvina Reynolds
+    Robert Pete Williams
+    Alice Stuart Thomas
+    Bess Lomax Hawes
+    Charles Seeger
 
-  3. Eighth Annual Berkeley Folk Music Festival (1965):
-    - No specific venue mentioned in the provided document.
-
-  Please note that the documents do not provide information on the venues used for the Fifth and Eighth Annual Berkeley Folk Music Festivals.
+    Unfortunately, the documents provided do not include information about musicians who performed at the Berkeley Folk Music Festival in other years during the 1960s or 1970s. Therefore, I can only confirm the musicians for the 1966 festival.
 
     {context}
 
