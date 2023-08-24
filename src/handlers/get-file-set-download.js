8fb73f54278a898c111725044634f8f06802221e
Karen Shaw <karendid@gmail.com>
2023-08-24 16:11:23 +0000

    Create MediaConvert job

--- src/handlers/get-file-set-download.js
+++ src/handlers/get-file-set-download.js
@@ -21,7 +21,7 @@ exports.handler = wrap(async (event) => {
   if (esResponse.statusCode == "200") {
     const doc = JSON.parse(esResponse.body)
     if (downloadAvailable(doc)) {
-      return await processDownload(doc._source.streaming_url, email);
+      return processDownload(doc._source.streaming_url, email);
     } else {
       return invalidRequest(405, "Download only allowed for role: Access, work_type: Video or Audio, with a valid streaming_url")
     }
@@ -41,24 +41,35 @@ function downloadAvailable(doc) {
   );
 }
 
-async function processDownload(streaming_url) {
+function processDownload(streaming_url, email) {
+  console.log("PROCESS DOWNLOAD")
   var stepfunctions = new AWS.StepFunctions({endpoint: 'http://172.17.0.1:8083'});
 
   var params = {
-    stateMachineArn: 'arn:aws:states:us-east-1:123456789012:stateMachine:HelloStepFunction', 
-    name: 'fromAPI',
+    stateMachineArn: 'arn:aws:states:us-east-1:123456789012:stateMachine:hlsStitcherStepFunction', 
+    input: JSON.stringify({s3Location:s3Location(streaming_url), email: email })
   };
-  await stepfunctions.startExecution(params, function(err, data) {
-    if (err) console.log(err, err.stack); 
-    else     console.log(data);           
-  }).promise();
+  stepfunctions.startExecution(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack); 
+      
+    }else {
+      console.log(data);           
+      
+    } 
+  })  
   return {
     statusCode: 200,
     headers: { "content-type": "text/plain" },
     body: JSON.stringify({
       message: `Creating download for ${streaming_url}. Check your email for a link.`,
     }),
-  };
+  }; 
+}
+
+function s3Location(_streaming_url){
+  // for now
+  return "s3://meadow-s-streaming/88/c1/59/6b/-8/02/2-/45/a3/-9/a4/e-/22/76/c3/f1/46/59/88c1596b-8022-45a3-9a4e-2276c3f14659.m3u8"
 }
 
 function invalidRequest(code, message) {
