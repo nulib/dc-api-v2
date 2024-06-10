bd4954e0485abc6a879cf2a112e6bb18c7c0fdee
Michael B. Klein <mbklein@gmail.com>
2024-06-10 23:37:09 +0000

    Remove try/catch from chat handler to let honeybadger handle it

--- chat/src/handlers/chat.py
+++ chat/src/handlers/chat.py
@@ -1,8 +1,6 @@
 import boto3
 import json
 import os
-import sys
-import traceback
 from datetime import datetime
 from event_config import EventConfig
 from helpers.response import prepare_response
@@ -18,43 +16,36 @@ RESPONSE_TYPES = {
 }
 
 def handler(event, context):
-    try:
-        config = EventConfig(event)
-        socket = event.get('socket', None)
-        config.setup_websocket(socket)
+    config = EventConfig(event)
+    socket = event.get('socket', None)
+    config.setup_websocket(socket)
 
-        if not config.is_logged_in:
-            config.socket.send({"type": "error", "message": "Unauthorized"})
-            return {"statusCode": 401, "body": "Unauthorized"}
-        
-        debug_message = config.debug_message()
-        if config.debug_mode:
-            config.socket.send(debug_message)
+    if not config.is_logged_in:
+        config.socket.send({"type": "error", "message": "Unauthorized"})
+        return {"statusCode": 401, "body": "Unauthorized"}
+    
+    debug_message = config.debug_message()
+    if config.debug_mode:
+        config.socket.send(debug_message)
 
-        if not os.getenv("SKIP_WEAVIATE_SETUP"):
-            config.setup_llm_request()
-            final_response = prepare_response(config)
-            config.socket.send(reshape_response(final_response, 'debug' if config.debug_mode else 'base'))
+    if not os.getenv("SKIP_WEAVIATE_SETUP"):
+        config.setup_llm_request()
+        final_response = prepare_response(config)
+        config.socket.send(reshape_response(final_response, 'debug' if config.debug_mode else 'base'))
 
-        log_group = os.getenv('METRICS_LOG_GROUP')
-        log_stream = context.log_stream_name
-        if log_group and ensure_log_stream_exists(log_group, log_stream):
-            log_client = boto3.client('logs')
-            log_message = reshape_response(final_response, 'log')
-            log_events = [
-                {
-                    'timestamp': timestamp(),
-                    'message': json.dumps(log_message)
-                }
-            ]
-            log_client.put_log_events(logGroupName=log_group, logStreamName=log_stream, logEvents=log_events)
-        return {"statusCode": 200}
-        
-    except Exception:
-        exc_info = sys.exc_info()
-        err_text = ''.join(traceback.format_exception(*exc_info))
-        print(err_text)
-        return {"statusCode": 500, "body": f'Unhandled error:\n{err_text}'}
+    log_group = os.getenv('METRICS_LOG_GROUP')
+    log_stream = context.log_stream_name
+    if log_group and ensure_log_stream_exists(log_group, log_stream):
+        log_client = boto3.client('logs')
+        log_message = reshape_response(final_response, 'log')
+        log_events = [
+            {
+                'timestamp': timestamp(),
+                'message': json.dumps(log_message)
+            }
+        ]
+        log_client.put_log_events(logGroupName=log_group, logStreamName=log_stream, logEvents=log_events)
+    return {"statusCode": 200}
 
 def reshape_response(response, type):
     return {k: response[k] for k in RESPONSE_TYPES[type]}
