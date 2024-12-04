b3b531592caa69a4882f783e4c29bcda741f6495
Michael B. Klein <mbklein@gmail.com>
2024-12-04 17:53:41 +0000

    Begin to add LangGraph agent code
    Cleanup model info from event config
    
    Get agents working printing to logs
    
    Add Azure API key to chat template
    
    wip: temporary log to chat metric group
    
    Fix secrets and build stuff in chat
    
    Get agent callbacks working
    
    Pass web socket to agent handler and add skeleton for callbacks
    
    Exclude embeddings from hybrid query
    
    Add websocket callbacks
    
    Add responses over websocket
    
    Send tool start/end, tokens, and stop/result messages over websocket
    
    Teach the chat handler how to forget on demand
    
    Update tools, remove examples
    
    Improve prompting
    Compress checkpoint data
    Add more fields back to hybrid query
    
    Add S3Saver and S3 infrastructure to chat template
    
    Rename source_documents to search_result

--- chat/src/handlers/chat.py
+++ chat/src/handlers/chat.py
@@ -1,79 +1,113 @@
-import secrets # noqa
+import secrets  # noqa
 import boto3
 import json
-import logging
 import os
+import traceback
 from datetime import datetime
 from event_config import EventConfig
-from helpers.response import Response
-from honeybadger import honeybadger
+# from honeybadger import honeybadger
+from agent.dynamodb_cleaner import delete_checkpoint
+from agent.search_agent import search_agent
+from langchain_core.messages import HumanMessage
+from agent.agent_handler import AgentHandler
 
-honeybadger.configure()
-logging.getLogger('honeybadger').addHandler(logging.StreamHandler())
+# honeybadger.configure()
+# logging.getLogger("honeybadger").addHandler(logging.StreamHandler())
 
 RESPONSE_TYPES = {
     "base": ["answer", "ref"],
-    "debug": ["answer", "attributes", "azure_endpoint", "deployment_name", "is_superuser", "k", "openai_api_version", "prompt", "question", "ref", "temperature", "text_key", "token_counts"],
-    "log": ["answer", "deployment_name", "is_superuser", "k", "openai_api_version", "prompt", "question", "ref", "size", "source_documents", "temperature", "token_counts", "is_dev_team"],
-    "error": ["question", "error", "source_documents"]
+    "debug": [
+        "answer",
+        "attributes",
+        "azure_endpoint",
+        "deployment_name",
+        "is_superuser",
+        "k",
+        "openai_api_version",
+        "prompt",
+        "question",
+        "ref",
+        "temperature",
+        "text_key",
+        "token_counts",
+    ],
+    "log": [
+        "answer",
+        "deployment_name",
+        "is_superuser",
+        "k",
+        "openai_api_version",
+        "prompt",
+        "question",
+        "ref",
+        "size",
+        "source_documents",
+        "temperature",
+        "token_counts",
+        "is_dev_team",
+    ],
+    "error": ["question", "error", "source_documents"],
 }
+
 
 def handler(event, context):
     config = EventConfig(event)
-    socket = event.get('socket', None)
+    socket = event.get("socket", None)
     config.setup_websocket(socket)
 
     if not (config.is_logged_in or config.is_superuser):
         config.socket.send({"type": "error", "message": "Unauthorized"})
         return {"statusCode": 401, "body": "Unauthorized"}
 
+    if config.forget:
+        delete_checkpoint(config.ref)
+
     if config.question is None or config.question == "":
         config.socket.send({"type": "error", "message": "Question cannot be blank"})
         return {"statusCode": 400, "body": "Question cannot be blank"}
-    
-    debug_message = config.debug_message()
-    if config.debug_mode:
-        config.socket.send(debug_message)
 
-    if not os.getenv("SKIP_LLM_REQUEST"):
-        config.setup_llm_request()
-
-    response = Response(config)
-    final_response = response.prepare_response()
-    if "error" in final_response:
-        logging.error(f'Error: {final_response["error"]}')
-        config.socket.send({"type": "error", "message": "Internal Server Error"})
-        return {"statusCode": 500, "body": "Internal Server Error"}
-    else:
-        config.socket.send(reshape_response(final_response, 'debug' if config.debug_mode else 'base'))
-
-    log_group = os.getenv('METRICS_LOG_GROUP')
+    log_group = os.getenv("METRICS_LOG_GROUP")
     log_stream = context.log_stream_name
     if log_group and ensure_log_stream_exists(log_group, log_stream):
-        log_client = boto3.client('logs')
-        log_message = reshape_response(final_response, 'log')
-        log_events = [
-            {
-                'timestamp': timestamp(),
-                'message': json.dumps(log_message)
-            }
-        ]
-        log_client.put_log_events(logGroupName=log_group, logStreamName=log_stream, logEvents=log_events)
+        log_client = boto3.client("logs")
+        log_events = [{"timestamp": timestamp(), "message": "Hello world"}]
+    log_client.put_log_events(
+            logGroupName=log_group, logStreamName=log_stream, logEvents=log_events
+        )
+
+    callbacks = [AgentHandler(config.socket, config.ref)]
+    try:
+        search_agent.invoke(
+            {"messages": [HumanMessage(content=config.question)]},
+            config={"configurable": {"thread_id": config.ref}, "callbacks": callbacks, "metadata": {"model_deployment": os.getenv("AZURE_OPENAI_LLM_DEPLOYMENT_ID")}},
+            debug=False
+        )
+    except Exception as e:
+        print(f"Error: {e}")
+        print(traceback.format_exc())
+        error_response = {"type": "error", "message": "An unexpected error occurred. Please try again later."}
+        if config.socket:
+            config.socket.send(error_response)
+        return {"statusCode": 500, "body": json.dumps(error_response)}
+
     return {"statusCode": 200}
+
 
 def reshape_response(response, type):
     return {k: response[k] for k in RESPONSE_TYPES[type]}
 
+
 def ensure_log_stream_exists(log_group, log_stream):
-    log_client = boto3.client('logs')
+    log_client = boto3.client("logs")
     try:
         log_client.create_log_stream(logGroupName=log_group, logStreamName=log_stream)
         return True
     except log_client.exceptions.ResourceAlreadyExistsException:
         return True
     except Exception:
-        print(f'Could not create log stream: {log_group}:{log_stream}')
+        print(f"Could not create log stream: {log_group}:{log_stream}")
         return False
+
 
 def timestamp():
     return round(datetime.timestamp(datetime.now()) * 1000)
