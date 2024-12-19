abb1eb230643cc0df9c36852209fac7bc33c1768
Michael B. Klein <mbklein@gmail.com>
2024-12-19 18:59:09 +0000

    Change node coverage thresholds

--- node/nyc.config.js
+++ node/nyc.config.js
@@ -4,6 +4,16 @@ const defaultExclude = require('@istanbuljs/schema/default-exclude');
 const localExclude = [".aws-sam/**/*", "docs/**/*"];
 module.exports = {
   all: true,
+  branches: 80,
+  lines: 80,
+  functions: 80,
+  statements: 80,
+  watermarks: {
+    lines: [80, 90],
+    functions: [80, 90],
+    branches: [80, 90],
+    statements: [80, 90]
+  },
   "check-coverage": true,
   exclude: defaultExclude.concat(localExclude)
 };
