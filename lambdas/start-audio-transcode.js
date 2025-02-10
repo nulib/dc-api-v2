bbcf04ea25b7d4c9b9e76bacf7003f7dbe053097
Karen Shaw <karendid@gmail.com>
2025-02-10 18:55:15 +0000

    Update av download lambda to node 22.x and AWS SDK v3

--- lambdas/start-audio-transcode.js
+++ lambdas/start-audio-transcode.js
@@ -1,41 +1,39 @@
-const AWS = require('aws-sdk');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 const ffmpeg = require('fluent-ffmpeg');
 const stream = require('stream');
 
 module.exports.handler = async (event) => {
-  
-  const s3 = new AWS.S3();
-  const url = event.streamingUrl;
-  const referer = event.referer || null;
-  const inputOptions = referer ? ["-referer", referer] : [];
-  const bucket = event.destinationBucket;
-  const key = event.destinationKey;
-  const pass = new stream.PassThrough();
+ const s3Client = new S3Client();
+ const url = event.streamingUrl;
+ const referer = event.referer || null;
+ const inputOptions = referer ? ["-referer", referer] : [];
+ const bucket = event.destinationBucket;
+ const key = event.destinationKey;
+ const pass = new stream.PassThrough();
 
-  return new Promise((resolve, reject) => {
-    ffmpeg()
-      .input(url)
-      .inputOptions(inputOptions)
-      .format('mp3')
-      .output(pass, { end: true })
-      .on('start', () => {
-        s3.upload({
-          Bucket: bucket,
-          Key: key,
-          Body: pass,
-        }, (error, _data) => {
-          if (error) {
-            console.error('upload failed', error);
-            reject(error);
-          } else {
-            resolve({ success: true });
-          }
-        });
-      })
-      .on('error', (error) => {
-        console.error('ffmpeg error', error);
-        reject(error);
-      })
-      .run();
-  });
+ return new Promise((resolve, reject) => {
+   ffmpeg()
+     .input(url)
+     .inputOptions(inputOptions)
+     .format('mp3')
+     .output(pass, { end: true })
+     .on('start', async () => {
+       try {
+         await s3Client.send(new PutObjectCommand({
+           Bucket: bucket,
+           Key: key,
+           Body: pass
+         }));
+         resolve({ success: true });
+       } catch (error) {
+         console.error('upload failed', error);
+         reject(error);
+       }
+     })
+     .on('error', (error) => {
+       console.error('ffmpeg error', error);
+       reject(error);
+     })
+     .run();
+ });
 }
