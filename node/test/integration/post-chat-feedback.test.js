3f9f4a198d08f793493b3aab9d89c810a036ff10
charlesLoder <charles.loder@northwestern.edu>
2025-03-14 13:28:31 +0000

    Update feedback validation and naming

--- node/test/integration/post-chat-feedback.test.js
+++ node/test/integration/post-chat-feedback.test.js
@@ -32,16 +32,25 @@ describe("Chat feedback route", () => {
     it("should return 401 if user is not logged in", async () => {
       let requestBody = JSON.stringify({
         sentiment: "positive",
+        timestamp: new Date().toISOString(),
+        ref: "5a6e1d76-0d4c-43c5-ab2c-4687112ba102",
+        refIndex: 0,
         context: {
           ref: "5a6e1d76-0d4c-43c5-ab2c-4687112ba102",
-          question: "What is the capital of France?",
-          answer: "Paris",
-          source_documents: ["https://doc1", "https://doc2"],
+          initialQuestion: "What is the capital of France?",
+          turns: [
+            {
+              question: "What is the capital of France?",
+              answer: "Paris",
+              works: [],
+              aggregations: [],
+            },
+          ],
         },
         feedback: {
-          options: ["option1"],
-          text: "Great answer!",
-          email: "user@example.com",
+          options: [],
+          text: "",
+          email: "",
         },
       });
 
@@ -54,21 +63,30 @@ describe("Chat feedback route", () => {
       expect(response.body).to.equal("Authorization Required");
     });
 
-    it("should fail if request body is invalid", async () => {
+    it("should fail if sentiment is invalid", async () => {
       const token = new ApiToken().user({ uid: "abc123" }).sign();
 
       let requestBody = JSON.stringify({
         sentiment: "neutral",
+        timestamp: new Date().toISOString(),
+        ref: "e6005d7c-e03b-43f7-94a3-e327b4b5a538",
+        refIndex: 0,
         context: {
           ref: "3fc98004-995b-4491-94fd-aea48a0363ba",
-          question: "What is the capital of France?",
-          answer: "Paris",
-          source_documents: ["https://doc1", "https://doc2"],
+          initialQuestion: "What is the capital of France?",
+          turns: [
+            {
+              question: "What is the capital of France?",
+              answer: "Paris",
+              works: [],
+              aggregations: [],
+            },
+          ],
         },
         feedback: {
-          options: ["option1"],
-          text: "Great answer!",
-          email: "user@example.com",
+          options: [],
+          text: "",
+          email: "",
         },
       });
 
@@ -82,8 +100,119 @@ describe("Chat feedback route", () => {
       const response = await handler(event);
       expect(response.statusCode).to.equal(400);
       expect(response.body).to.equal(
-        `"sentiment is not one of enum values: positive,negative"`
+        `sentiment is not one of enum values: positive,negative`
       );
+    });
+
+    it("should fail if ref is missing", async () => {
+      const token = new ApiToken().user({ uid: "abc123" }).sign();
+      let requestBody = JSON.stringify({
+        sentiment: "positive",
+        timestamp: new Date().toISOString(),
+        // ... we omit ref here ...
+        refIndex: 0,
+        context: {
+          ref: "e6005d7c-e03b-43f7-94a3-e327b4b5a538",
+          initialQuestion: "Hello?",
+          turns: [
+            {
+              question: "Hello?",
+              answer: "World",
+              works: [],
+              aggregations: [],
+            },
+          ],
+        },
+        feedback: {
+          options: [],
+          text: "",
+          email: "",
+        },
+      });
+      const event = helpers
+        .mockEvent("POST", "/chat-feedback")
+        .body(requestBody)
+        .headers({
+          Cookie: `${process.env.API_TOKEN_NAME}=${token}`,
+        })
+        .render();
+      const response = await handler(event);
+      expect(response.statusCode).to.equal(400);
+      expect(response.body).to.equal(`instance requires property "ref"`);
+    });
+
+    it("should fail if refIndex is missing", async () => {
+      const token = new ApiToken().user({ uid: "abc123" }).sign();
+      let requestBody = JSON.stringify({
+        sentiment: "positive",
+        timestamp: new Date().toISOString(),
+        ref: "e6005d7c-e03b-43f7-94a3-e327b4b5a538",
+        // ... we omit refIndex ...
+        context: {
+          ref: "e6005d7c-e03b-43f7-94a3-e327b4b5a538",
+          initialQuestion: "Hello?",
+          turns: [
+            {
+              question: "Hello?",
+              answer: "World",
+              works: [],
+              aggregations: [],
+            },
+          ],
+        },
+        feedback: {
+          options: [],
+          text: "",
+          email: "",
+        },
+      });
+      const event = helpers
+        .mockEvent("POST", "/chat-feedback")
+        .body(requestBody)
+        .headers({
+          Cookie: `${process.env.API_TOKEN_NAME}=${token}`,
+        })
+        .render();
+      const response = await handler(event);
+      expect(response.statusCode).to.equal(400);
+      expect(response.body).to.equal(`instance requires property "refIndex"`);
+    });
+
+    it("should fail if timestamp is missing", async () => {
+      const token = new ApiToken().user({ uid: "abc123" }).sign();
+      let requestBody = JSON.stringify({
+        sentiment: "positive",
+        // ... we omit timestamp ...
+        ref: "e6005d7c-e03b-43f7-94a3-e327b4b5a538",
+        refIndex: 0,
+        context: {
+          ref: "e6005d7c-e03b-43f7-94a3-e327b4b5a538",
+          initialQuestion: "Hello?",
+          turns: [
+            {
+              question: "Hello?",
+              answer: "World",
+              works: [],
+              aggregations: [],
+            },
+          ],
+        },
+        feedback: {
+          options: [],
+          text: "",
+          email: "",
+        },
+      });
+      const event = helpers
+        .mockEvent("POST", "/chat-feedback")
+        .body(requestBody)
+        .headers({
+          Cookie: `${process.env.API_TOKEN_NAME}=${token}`,
+        })
+        .render();
+      const response = await handler(event);
+      expect(response.statusCode).to.equal(400);
+      expect(response.body).to.equal(`instance requires property "timestamp"`);
     });
 
     describe("Saving feedback", () => {
@@ -92,11 +221,20 @@ describe("Chat feedback route", () => {
 
         const requestBody = {
           sentiment: "negative",
+          timestamp: new Date().toISOString(),
+          ref: "e6005d7c-e03b-43f7-94a3-e327b4b5a538",
+          refIndex: 0,
           context: {
             ref: "e6005d7c-e03b-43f7-94a3-e327b4b5a538",
-            question: "What is the capital of France?",
-            answer: "Rome",
-            source_documents: ["https://doc1", "https://doc2"],
+            initialQuestion: "What is the capital of France?",
+            turns: [
+              {
+                question: "What is the capital of France?",
+                answer: "Rome",
+                works: [],
+                aggregations: [],
+              },
+            ],
           },
           feedback: {
             options: ["option1"],
@@ -126,7 +264,7 @@ describe("Chat feedback route", () => {
           process.env.CHAT_FEEDBACK_BUCKET
         );
         expect(s3Mock.call(0).args[0].input.Key).eq(
-          "negative/e6005d7c-e03b-43f7-94a3-e327b4b5a538.json"
+          "negative/e6005d7c-e03b-43f7-94a3-e327b4b5a538_0.json"
         );
         expect(s3Mock.call(0).args[0].input.ContentType).eq("application/json");
         expect(JSON.parse(s3Mock.call(0).args[0].input.Body)).to.deep.equal(
